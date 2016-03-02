cc3cd6e18f00376323f344b436a3d5c372b94e5b
Anthony Garant <agarant@gmail.com>
2016-03-02 10:11:56 -0500

    Add classifier builder

---
+++ mldb/classifier_builder.py
@@ -0,0 +1,21 @@
+def build_from_file(mldb, name, model):
+    mldb.put("/v1/functions/" + name, {
+        "type": "classifier",
+        "params": {
+            "modelFileUrl": "file:///mldb_data/" + model
+        }
+    })
+
+
+def build_endpoint(mldb, name, classifier, words_filters, feature_name, ngram_range):
+    features = "tokenize(" + feature_name + ", {splitchars:' ', quotechar:'\n', ngram_range:[" + ngram_range + "]})"
+    for words_filter in words_filters:
+        features = words_filter + "({words:{" + features + "}})"
+
+    mldb.put("/v1/functions/" + name, {
+        "type": "sql.expression",
+        "params": {
+            "expression": classifier + "(" + features + ")",
+            "prepared": "true"
+        }
+    })
--- mldb/request.py
+++ mldb/request.py
@@ -3,8 +3,11 @@ import helper_functions as helpers
 import training_functions as trainers
 import transform_procedures as transformers
 import datasets_builder as datasets_builder
+import classifier_builder as classifier_builder
 
 MLDB_ADDRESS = "http://localhost:8080"
+LABEL_COL = 'spoiler'
+FEATURE_COL = 'sentence'
 
 mldb = Connection(MLDB_ADDRESS)
 
@@ -13,7 +16,7 @@ stemmer = 'english_stemmer'
 words_filter = 'stopwords_filter'
 helpers.create_stemmer(mldb, stemmer, 'english')
 helpers.create_filter_stopwords(mldb, words_filter)
-filters = [stemmer, words_filter]
+words_filters = []
 
 """ Build the datasets that we will need to train our models """
 spoilers_balanced_ds = 'spoilers_balanced'
@@ -21,12 +24,20 @@ datasets_builder.build_from_file(mldb, spoilers_balanced_ds, 'series_balanced.cs
 
 """ Transform our dataset to have it in a trainable format """
 spoilers_balanced_ds_ts = 'spoilers_balanced_split'
-transformers.baggify(mldb, 'spoiler_bag_of_word', 'spoiler', 'sentence', spoilers_balanced_ds, spoilers_balanced_ds_ts,
-                     filters, '1,2')
+transformers.baggify(mldb, 'spoiler_bag_of_word', LABEL_COL, FEATURE_COL, spoilers_balanced_ds, spoilers_balanced_ds_ts,
+                     words_filters, '1,2')
 
 """ Train models with the transformed datasets """
-spoilers_balanced_md = 'spoilers_classifier'
-md = trainers.classifier_experiment(mldb, spoilers_balanced_md, spoilers_balanced_ds_ts, 'model_experiment.cls', 'bbdt')
+spoilers_balanced_fn = 'spoilers_classifier'
+spoilers_balanced_md = 'model_experiment.cls'
+md = trainers.classifier_experiment(mldb, spoilers_balanced_fn, spoilers_balanced_ds_ts, spoilers_balanced_md, 'bbdt')
 
 auc = md.json()["status"]["firstRun"]["status"]["folds"][0]["results"]["auc"]
 print "\nArea under ROC curve = %0.4f\n" % auc
+
+
+""" Create the classifier enpoints """
+spoilers_classifier_fn = 'spoilers_classifier_fn'
+spoilers_classifier_endpoint = 'spoilers_classifier_api'
+classifier_builder.build_from_file(mldb, spoilers_classifier_fn, spoilers_balanced_md)
+classifier_builder.build_endpoint(mldb, spoilers_classifier_endpoint, spoilers_classifier_fn, words_filters, FEATURE_COL, '1,2')
